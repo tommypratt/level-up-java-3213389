57c2609a5cd15834c1142a54a2e765aacc5c28ad
tommypratt <89829340+tommypratt@users.noreply.github.com>
2022-11-21 23:48:25 +0000

    Committting

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,13 +1,19 @@
 package com.linkedin.javacodechallenges;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Please enter a number: ");
+        int input = reader.nextInt();
+
+        System.out.println("Is number even? " + IsEven(input));
+        reader.close();
+    }
+
+    public static boolean IsEven(int input) {
+        return input % 2 == 0;
     }
 }
--- javacodechallenges/src/test/java/com/linkedin/javacodechallenges/AppTest.java
+++ javacodechallenges/src/test/java/com/linkedin/javacodechallenges/AppTest.java
@@ -1,5 +1,6 @@
 package com.linkedin.javacodechallenges;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -7,14 +8,23 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        assertTrue(App.IsEven(4));
+        assertTrue(App.IsEven(2));
+        assertTrue(App.IsEven(-4));
+        assertTrue(App.IsEven(-2));
+    }
+
+    @Test
+    public void shouldAnswerWithFalse() {
+        assertFalse(App.IsEven(1));
+        assertFalse(App.IsEven(3));
+        assertFalse(App.IsEven(-1));
+        assertFalse(App.IsEven(-23));
     }
 }
