6715c7d7964120dd68b6234e2c6c8d2646a84a2c
tommypratt <89829340+tommypratt@users.noreply.github.com>
2022-11-22 22:46:58 +0000

    Implement Calculate Water Bill

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -4,64 +4,89 @@ import java.util.Scanner;
 
 public class App {
 
-    public static boolean isEven(int input) {
-        return input % 2 == 0;
-    }
-
-    /*
-     * code for odd or even main event
-     * public static void main(String[] args) {
-     * Scanner reader = new Scanner(System.in);
-     * 
-     * System.out.println("Please enter a number: ");
-     * int input = reader.nextInt();
-     * 
-     * System.out.println("Is number even? " + isEven(input));
-     * reader.close();
-     * }
-     */
-
-    public static boolean isPasswordComplex(String input) {
-        return input.length() >= 6
-                && input.matches(".*\\d.*") // find number
-                && input.matches(".*[a-z].*") // find lowercase
-                && input.matches(".*[A-Z].*"); // find uppercase
-
-        // long way vs regex way
-        // int lower = 0, upper = 0, numeric = 0;
-
-        // if (input.length() < 6)
-        // return false;
-
-        // for (char i : input.toCharArray()) {
-        // if (Character.isUpperCase(i))
-        // upper++;
-        // else if (Character.isLowerCase(i))
-        // lower++;
-        // else if (Character.isDigit(i))
-        // numeric++;
-        // }
-
-        // if (upper == 0 || lower == 0 || numeric == 0)
-        // return false;
-
-        // return true;
-    }
-
-
-    // public static void main(String[] args) {
-    //     Scanner reader = new Scanner(System.in);
-
-    //     System.out.println("Enter a Password: ");
-    //     String input = reader.nextLine();
-
-    //     System.out.println("Is the password complex? " + isPasswordComplex(input));
-    //     reader.close();
+    // *********START CHALLENGE ODD OR EVEN
+    // public static boolean isEven(int input) {
+    // return input % 2 == 0;
     // }
 
-    public static void main(String[] args) {
-        DoubleOrNothing game = new DoubleOrNothing();
-        game.playGame();
+    // public static void main(String[] args) {
+    // Scanner reader = new Scanner(System.in);
 
+    // System.out.println("Please enter a number: ");
+    // int input = reader.nextInt();
+
+    // System.out.println("Is number even? " + isEven(input));
+    // reader.close();
+    // }
+    /***** END OF ODD OR EVEN */
+
+    // ******START CHALLENGE PASSWORD COMPLEXITY */
+    // public static boolean isPasswordComplex(String input) {
+    // return input.length() >= 6
+    // && input.matches(".*\\d.*") // find number
+    // && input.matches(".*[a-z].*") // find lowercase
+    // && input.matches(".*[A-Z].*"); // find uppercase
+
+    // long way vs regex way
+    // int lower = 0, upper = 0, numeric = 0;
+
+    // if (input.length() < 6)
+    // return false;
+
+    // for (char i : input.toCharArray()) {
+    // if (Character.isUpperCase(i))
+    // upper++;
+    // else if (Character.isLowerCase(i))
+    // lower++;
+    // else if (Character.isDigit(i))
+    // numeric++;
+    // }
+
+    // if (upper == 0 || lower == 0 || numeric == 0)
+    // return false;
+
+    // return true;
+    // }
+
+    // public static void main(String[] args) {
+    // Scanner reader = new Scanner(System.in);
+
+    // System.out.println("Enter a Password: ");
+    // String input = reader.nextLine();
+
+    // System.out.println("Is the password complex? " + isPasswordComplex(input));
+    // reader.close();
+    // }
+    // ******END PASSWORD COMPLEXITY */
+
+    // *******START CHALLENGE DOUBLE OR NOTHING */
+    // public static void main(String[] args) {
+    // DoubleOrNothing game = new DoubleOrNothing();
+    // game.playGame();
+
+    // }
+    // ******END DOUBLE OR NOTHING */
+
+    //*****START CHALLENGE CALCULATE WATER BILL */
+
+    public static double calculateWaterBill(double gallonsUsage)
+    {
+        double amountOwing = 18.84;
+        double ccf = gallonsUsage / 748;
+
+        if(ccf - 2 > 0)
+        {
+            amountOwing = (Math.ceil(ccf - 2)) * 3.90 + amountOwing; 
+        }
+
+        return amountOwing;
+    }
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("How many gallons of water did you use this month?");
+        double usage = reader.nextDouble();
+        System.out.println("Your waterbill is $" + calculateWaterBill(usage));
+        reader.close();
     }
 }
--- javacodechallenges/src/test/java/com/linkedin/javacodechallenges/AppTest.java
+++ javacodechallenges/src/test/java/com/linkedin/javacodechallenges/AppTest.java
@@ -1,5 +1,6 @@
 package com.linkedin.javacodechallenges;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -11,26 +12,34 @@ import org.junit.Test;
 public class AppTest {
 
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(App.isEven(4));
-        assertTrue(App.isEven(2));
-        assertTrue(App.isEven(-4));
-        assertTrue(App.isEven(-2));
-        assertTrue(App.isPasswordComplex("12zsaD"));
-        assertTrue(App.isPasswordComplex("ddzs3aD"));
-        assertTrue(App.isPasswordComplex("ddzDsa2"));
-        assertTrue(App.isPasswordComplex("3zsaDf"));
+    public void calculateWaterBill(){
+        assertEquals(22.74, App.calculateWaterBill(1800), 0);
+        assertEquals(22.74, App.calculateWaterBill(2244), 0);
+        assertEquals(26.64, App.calculateWaterBill(2245), 0);
+        assertEquals(46.14, App.calculateWaterBill(6000), 0);
     }
 
-    @Test
-    public void shouldAnswerWithFalse() {
-        assertFalse(App.isEven(1));
-        assertFalse(App.isEven(3));
-        assertFalse(App.isEven(-1));
-        assertFalse(App.isEven(-23));
-        assertFalse(App.isPasswordComplex("12zaD"));
-        assertFalse(App.isPasswordComplex("ddzsdaD"));
-        assertFalse(App.isPasswordComplex("ddzsa1"));
-        assertFalse(App.isPasswordComplex("ALLUPPER1"));
-    }
+    // @Test
+    // public void shouldAnswerWithTrue() {
+    // assertTrue(App.isEven(4));
+    // assertTrue(App.isEven(2));
+    // assertTrue(App.isEven(-4));
+    // assertTrue(App.isEven(-2));
+    // assertTrue(App.isPasswordComplex("12zsaD"));
+    // assertTrue(App.isPasswordComplex("ddzs3aD"));
+    // assertTrue(App.isPasswordComplex("ddzDsa2"));
+    // assertTrue(App.isPasswordComplex("3zsaDf"));
+    // }
+
+    // @Test
+    // public void shouldAnswerWithFalse() {
+    // assertFalse(App.isEven(1));
+    // assertFalse(App.isEven(3));
+    // assertFalse(App.isEven(-1));
+    // assertFalse(App.isEven(-23));
+    // assertFalse(App.isPasswordComplex("12zaD"));
+    // assertFalse(App.isPasswordComplex("ddzsdaD"));
+    // assertFalse(App.isPasswordComplex("ddzsa1"));
+    // assertFalse(App.isPasswordComplex("ALLUPPER1"));
+    // }
 }
